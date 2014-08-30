0e741410c13c163ad557c566bdd0f9605df20c7e
Quinn Rohlf <qr@qrohlf.com>
2014-08-29 19:51:38 -0700

    getting started

---
+++ Gemfile
@@ -0,0 +1,24 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'https://rubygems.org'
+source 'https://rails-assets.org'
+
+gem "middleman", "~>3.3.5"
+
+# Live-reloading plugin
+gem "middleman-livereload", "~> 3.1.0"
+
+# For faster file watcher updates on Windows:
+gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+
+# Windows does not come with time zone data
+gem "tzinfo-data", platforms: [:mswin, :mingw]
+
+# jQuery!
+gem 'jquery-rails', require: false
+
+# Bootstrap!
+gem "bootstrap-sass", require: false
+
+# Charts!
+gem "rails-assets-chartjs", require: false
---
+++ source/javascripts/application.js
@@ -0,0 +1,30 @@
+//= require jquery
+//= require bootstrap-sprockets
+//= require chartjs
+//= require_tree .
+
+// var data = [
+//     {
+//         value: 300,
+//         color:"#F7464A",
+//         highlight: "#FF5A5E",
+//         label: "Red"
+//     },
+//     {
+//         value: 50,
+//         color: "#46BFBD",
+//         highlight: "#5AD3D1",
+//         label: "Green"
+//     },
+//     {
+//         value: 100,
+//         color: "#FDB45C",
+//         highlight: "#FFC870",
+//         label: "Yellow"
+//     }
+// ]
+// $(document).ready( function() {
+//     var ctx = document.getElementById("language-chart").getContext("2d");
+//     var myDoughnutChart = new Chart(ctx).Doughnut(data, {});
+//   }
+// )
